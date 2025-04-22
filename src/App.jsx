b560ce35fe9369b408554b8078724bbabc215dcf
@swastik1456 <swastik@grktraining.com>
2025-04-22 18:49:42 +0530

    new branch & code uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>DEVOPS CLASS</h1>
+      <h1>DEVOPS CL</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
