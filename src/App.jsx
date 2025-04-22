7d01035f12a142c9a22a3a44bf429138ad3b1d07
chaitanya9907 <chaitanyamudimadugu@gmail.com>
2025-04-22 19:11:37 +0530

    Update App.jsx
    changes have been updated for pull Request

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>DEVOPS CL</h1>
+      <h1>DEVOPS CLASS EVENING BATCH</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
